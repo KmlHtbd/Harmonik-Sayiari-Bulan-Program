46944cab36aa191d7c5616aa5e5d6b53dcc9428b
kml htb <kmlhtbdev@gmail.com>
2022-10-16 16:46:48 +0300

    Initial commit

---
+++ src/harmonik.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class harmonik {
+    public static void main(String[] args) {
+        int n;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Bir sayı giriniz:");
+        n = input.nextInt();
+
+        double i, result=0 ;
+        for (i=1; i<=n ;i++){
+            result += (1/i);
+        }
+        System.out.print("Sonuç: "+result);
+    }
+}
